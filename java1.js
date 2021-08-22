6df7e546ae5b4c557677ffbbb9b4035d6833e3df
dohuukien-fif <huukien0099@gmail.com>
2021-08-22 15:13:07 +0700

    first commit

---
+++ java1.js
@@ -0,0 +1,112 @@
+/*function tongAB(a, b){
+    var t = a / b;
+    return t;
+}
+    try{
+var kq=tongAB(50, 1);
+alert(kq);
+    }catch(e){ }
+    alert("sai");
+   */
+ /*var a = 5;
+  function tongAB(_thamsoA , _thamsoB){
+var tong=(_thamsoA - _thamsoB + a)
+return tong;
+  }
+  alert(15,5);
+*/
+ /*function chao(){
+     alert("sản phẩm không có")
+ }*/
+ /*function changeImage(id){
+     let images = document.getElementById(id).getAttribute('src');
+     document.getElementById('maing').setAttribute('src',images)
+ }*/
+ /*function goe(){
+    alert("chao đón bạn");
+}  */
+
+/*var a = 5;
+var b = 8;
+if(a>b){
+    alert("cmm");
+}else{
+    alert("vvv");
+}*/
+/*function date(){
+var i;
+var tong = 0;
+for (i=1; i <= 100;i++){
+    tong +=i;
+}
+alert(tong);
+}*/
+/*var a =new Date().getMinutes();
+alert(a);*/
+ /* <script>
+if (new Date().getHours() >18 ){
+alert('chào ngày mới');
+}else{
+  alert("chào ngày cũ");
+}
+  </script> */
+/*var day;
+switch (new Date().getDay ()){
+case 0:
+    day = "sunday";
+    break;
+case 1:
+    day = "monday";
+    break;
+case 2:
+    day = "tuesday";
+    break;
+    case 3:
+    day = "wednesday";
+    break;
+    case 4:
+    day = "thurday";
+    break;
+    case 5:
+    day = "friday";
+    break;
+    case 6:
+    day = "saturday";
+  
+}
+    alert(day);*/
+
+/*function tinhtong(){
+    var a = document.getElementById("a");
+    var b =document.getElementById("b");
+    var tong=parseInt(a.value)+parseInt(b.value);
+    var kq =document.getElementById("kq")
+    kq.value = tong;
+    if(!NaN(tong)){
+        kq.value = tong;
+    }
+}*/
+/*function run(name, year_of_birth) {
+	var d = new Date();
+	var current_year = d.getFullYear();
+	// define a object 
+	var obj = {
+		name:name;
+		year_of_birth:year_of_birth;
+		address:"Ha Noi";
+	}
+
+	// Calculate age
+	age = current_year - year_of_birth - 1;
+	
+	console.log(obj); // print object
+	console.log(name + " is " + age + " years old");
+}*/
+var docomentt = document.getElementById("main-images");
+document.write(docomentt);
+
+
+
+
+
+   
---
+++ wedjs.js
@@ -0,0 +1,13 @@
+var buton = document.getElementsByClassName(".header-none__button");
+var m = "200px";
+function con() {
+  document.getElementById("header-nav__none").style.left = " 0px";
+}
+function none() {
+  document.getElementById("header-nav__none").style.left = "-120%";
+}
+var btn = document.querySelector(".btnn");
+var m = document.querySelector(".mm");
+function clc() {
+  btn.innerHTML = "kiên dep trai";
+}
